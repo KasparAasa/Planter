26b9174563d3667f7121032058ca12905f52f4f9
Kaspar <kasparaasa@gmail.com>
2023-05-11 15:14:12 +0200

    added plant dropdown.

--- src/pages/Plants.tsx
+++ src/pages/Plants.tsx
@@ -1,8 +1,8 @@
 import {Layout} from '@/components/Layout'
-import Link from 'next/link'
 import React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
-import {Cross2Icon} from '@radix-ui/react-icons'
+// import {Cross2Icon} from '@radix-ui/react-icons'
+import * as Collapsible from '@radix-ui/react-collapsible'
 
 export default function Plants() {
 
@@ -24,11 +24,93 @@ export default function Plants() {
             </Dialog.Portal>
           </Dialog.Root>
         </div>
-        <div>
-          <div>
 
-          </div>
-        </div>
+        <Collapsible.Root>
+          <Collapsible.Trigger asChild>
+            <div className={'flex border-gray-200 border-2'}>
+              <div>
+                <img className={'w-24'} src={'plantPlaceholder.jpeg'}/>
+              </div>
+
+              <div className={'flex justify-between w-full items-center'}>
+                <div className={'flex flex-col ml-3'}>
+                  <div>Plant name</div>
+                  <div>Plant nickname</div>
+                  <div>Room</div>
+                </div>
+                <div className={'flex justify-evenly flex-wrap mr-5 gap-6'}>
+                  <div className={'flex'}>LW</div>
+                  <div className={'flex'}>LF</div>
+                  <div className={'flex'}>LRP</div>
+                </div>
+              </div>
+            </div>
+          </Collapsible.Trigger>
+          <Collapsible.Content>
+            <div className={'flex border-gray-200 border-2 border-t-0 flex-col'}>
+              <div className={'flex m-2 gap-2'}>
+                <div>
+                  plantname requires medium to medium-high light. Place it in a well lighted area
+                  without direct sunlight.
+                </div>
+                <div>
+                  plantname has medium-low water needs. Let the soil dry out completely between
+                  watering.
+                </div>
+              </div>
+              <div className={'flex gap-1'}>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+              </div>
+            </div>
+          </Collapsible.Content>
+        </Collapsible.Root>
+
+        <Collapsible.Root>
+          <Collapsible.Trigger asChild>
+            <div className={'flex border-gray-200 border-2'}>
+              <div>
+                <img className={'w-24'} src={'plantPlaceholder.jpeg'}/>
+              </div>
+
+              <div className={'flex justify-between w-full items-center'}>
+                <div className={'flex flex-col ml-3'}>
+                  <div>Plant name</div>
+                  <div>Plant nickname</div>
+                  <div>Room</div>
+                </div>
+                <div className={'flex justify-evenly flex-wrap mr-5 gap-6'}>
+                  <div className={'flex'}>LW</div>
+                  <div className={'flex'}>LF</div>
+                  <div className={'flex'}>LRP</div>
+                </div>
+              </div>
+            </div>
+          </Collapsible.Trigger>
+          <Collapsible.Content>
+            <div className={'flex border-gray-200 border-2 border-t-0 flex-col'}>
+              <div className={'flex m-2 gap-2'}>
+                <div>
+                  plantname requires medium to medium-high light. Place it in a well lighted area
+                  without direct sunlight.
+                </div>
+                <div>
+                  plantname has medium-low water needs. Let the soil dry out completely between
+                  watering.
+                </div>
+              </div>
+              <div className={'flex gap-1'}>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+                <img className={'w-16'} src={'plantPlaceholder.jpeg'}/>
+              </div>
+            </div>
+          </Collapsible.Content>
+        </Collapsible.Root>
+
       </div>
     </Layout>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,28 @@ export default function Dashboard() {
 
   return (
     <Layout>
-      <div>
+      <div className={'flex border-gray-200 border-2'}>
         <div>
+          <img className={'h-20 w-20'} src={'plantPlaceholder.jpeg'}/>
         </div>
-        <div>
-        </div>
-        <div>
+        <div className={'flex justify-between w-full items-center'}>
+          <div className={'flex flex-col ml-2'}>
+            <div>Plant name</div>
+            <div>Plant nickname</div>
+            <div>Room</div>
+          </div>
+          <div className={'flex justify-evenly flex-wrap text-2xl'}>
+            <div className={'flex m-3'}>LW</div>
+            <div className={'flex m-3'}>LF</div>
+            <div className={'flex m-3'}>L-RP</div>
+          </div>
+          <div className={'flex flex-col pr-3 items-end'}>
+            <div className={''}>
+              Edit plant
+            </div>
+            <div className={''}>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
